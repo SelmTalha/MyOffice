c0899fee9cdd38ff97c1f6d8e03b379d9215207b
SelmTalha <selmtalha26@gmail.com>
2019-06-07 15:05:20 +0300

    07.06.19/15:05

---
+++ 07.06.19/dizilerveformat.py
@@ -0,0 +1,12 @@
+a = "Selim"
+b = "Çağlar"
+c = 26
+d = 3.141592
+
+e = ['a', 'b', 'c', 'd']
+
+print ("a={0}, b={1}, c={2}, d={3}".format(a, b, c, d))
+
+print ("{} {} {} {}".format(*e))
+
+print ("Merhaba {isim}".format(isim='Elif'))
